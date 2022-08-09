2b2f222262c24f7bb6df6c9d6235dc9fd1637b1c
R-Albuquerque <rafaelalbuquerque1447@gmail.com>
2022-08-08 22:01:14 -0300

    Tweaking the integration between GUI and back-end

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # py_sniffer
+
+This is a simple network packet sniffer with a GUI made in Python.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install PySimpleGUI.
+
+```bash
+pip install pysimplegui
+```
+
+## Usage
+
+Make sure to run as Administrator/sudo in order to access the network sockets.
+
+## Screenshot
+
+![screenshot](https://github.com/R-Albuquerque/py_sniffer/screenshots/sniff_gui.PNG)
+
+I will update this README in the future with a more detailed explanation of the inner workings.
--- sniffer_ipdecode_gui.py
+++ sniffer_ipdecode_gui.py
@@ -31,8 +31,8 @@ class IP_protocol(Structure):
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
 
         # endereço IP em forma legível
-        self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
+        self.source = socket.inet_ntoa(struct.pack("@I", self.src))
+        self.destination = socket.inet_ntoa(struct.pack("@I", self.dst))
 
         # obter o nome do protocolo em forma legível
         try:
@@ -40,48 +40,39 @@ class IP_protocol(Structure):
         except IndexError:
             self.protocol = str(self.protcol_num)
 
+hostname = socket.getfqdn()
+this_host = socket.gethostbyname_ex(hostname)[2][1]
 
-sg.theme('BlueMono')
+sg.theme('BrownBlue')
 
 
 config_col = [
     [
-        sg.Text("Host", justification='right', size=(25, 1), ),
-        sg.In(size=(25, 1), enable_events=True, key="-HOST-", default_text='192.168.0.7'),
+        sg.Text("Host:", justification='right', size=(25, 1), ),
+        sg.In(size=(25, 1), enable_events=True, key="-HOST-", default_text=this_host),
+        sg.Text("# of Packets:", justification='right', size=(10, 1), ),
+        sg.Combo([5,10,15], default_value=5, key='numPack'),
         sg.Button("Capture", size=(25, 1)),
 
     ],
     [
-        # sg.Listbox(
-        #     values=[], enable_events=True, size=(100,5), key="-FLIST-"
-        # )
-        sg.Table(headings=['Protocol', 'Source', 'Destination'], values=[], expand_x=True, enable_events=True, justification='c', key="-FLIST-")
+        sg.Table(headings=['Protocol', 'Source', 'Destination'], values=[], expand_x=True, enable_events=True, justification='c', key="-TABLE-")
     ]
 
 ]
-img_col = [
-    [sg.Text("Choose an image from list.", justification='right', size=(55, 1))],
-    [sg.Text(size=(25, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
-]
 
-# --- Full Layout ---
+# Layout da interface gráfica
 layout = [
     [
         sg.Column(config_col),
 
-    ],
-    # [sg.Column(result_col)],
-    [sg.HSeparator(), ],
-    [
-        sg.Column(img_col)
-    ],
+    ]
 ]
 
-window = sg.Window("Image Viewer", layout)
-
+window = sg.Window("Py Sniffer", layout)
+valores = []
 while True:
-    valores = []
+    
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
@@ -104,129 +95,24 @@ while True:
         # no windows mandaremos IOCTL para habilitar o modo promíscuo
         if os.name == "nt":
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
+        
         try:
             counter = 0
-            while counter < 10:
+            while counter < values['numPack']:
                 # Ler um pacote:
                 raw_buffer = sniffer.recvfrom(65535)[0]
 
                 # Obtendo o cabeçalho IP a partir dos primeiros 20 bytes do buffer:
                 ip_header = IP_protocol(raw_buffer[:20])
 
-                print("Protocol: %s %s -> %s" % (
-                    ip_header.protocol,
-                    ip_header.src_address,
-                    ip_header.dst_address)
-                )
-                valores.append([ip_header.protocol,ip_header.src_address,ip_header.dst_address])
-
-                window['-FLIST-'].Update(values=valores)
+                # Atualizando a tabela na Interface
+                window['-TABLE-'].Widget.insert('', 'end', values=[ip_header.protocol,ip_header.source,ip_header.destination])
+                read = True
                 counter += 1
 
         except KeyboardInterrupt:
-            # if we're on Windows turn off promiscuous mode
+            # Desligar o modo promíscuo da placa de rede no Windows
             if os.name == "nt":
                 sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-        # try:
-        #     file_list = os.listdir(folder)
-        # except:
-        #     file_list = []
-
-        # fnames = [
-        #     f
-        #     for f in file_list
-        #     if os.path.isfile(os.path.join(folder, f))
-        #     and f.lower().endswith((".png", ".gif"))
-        # ]
-        # window["-FLIST-"].update(fnames)
-
-    # elif event == "-FLIST-":
-    #     try:
-    #         filename = os.path.join(
-    #             values["-FOLDER-"], values["-FLIST-"][0]
-    #         )
-    #         window["-TOUT-"].update(filename)
-    #         window["-IMAGE-"].update(filename=filename)
-    #     except:
-    #         pass
 
 window.close()
-
-
-# =====================================================================================
-host = '192.168.0.7'
-
-# class IP_protocol(Structure):
-
-#     _fields_ = [
-#         ("ihl", c_ubyte, 4),
-#         ("version", c_ubyte, 4),
-#         ("tos", c_ubyte),
-#         ("len", c_ushort),
-#         ("id", c_ushort),
-#         ("offset", c_ushort),
-#         ("ttl", c_ubyte),
-#         ("protocol_num", c_ubyte),
-#         ("sum", c_ushort),
-#         ("src", c_uint32),
-#         ("dst", c_uint32)
-#     ]
-
-#     def __new__(cls, socket_buffer = None):
-#         return cls.from_buffer_copy(socket_buffer)
-
-#     def __init__(self, socket_buffer=None):
-#         self.socket_buffer = socket_buffer
-
-#         # mapeando as constantes dos protocolos aos nomes
-#         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
-
-#         # endereço IP em forma legível
-#         self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
-#         self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
-
-#         # obter o nome do protocolo em forma legível
-#         try:
-#             self.protocol = self.protocol_map[self.protocol_num]
-#         except IndexError:
-#             self.protocol = str(self.protcol_num)
-
-# Criando um raw socket e associando à interface pública
-if os.name == 'nt':
-    socket_protocol = socket.IPPROTO_IP
-else:
-    socket_protocol = socket.IPPROTO_ICMP
-
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-
-# Criando um raw socket e associando à interface pública
-sniffer.bind((host, 0))
-
-# Incluindo os IP HEADERS na captura
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-# no windows mandaremos IOCTL para habilitar o modo promíscuo
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-try:
-    counter = 0
-    while counter < 10:
-        # Ler um pacote:
-        raw_buffer = sniffer.recvfrom(65535)[0]
-
-        # Obtendo o cabeçalho IP a partir dos primeiros 20 bytes do buffer:
-        ip_header = IP_protocol(raw_buffer[:20])
-
-        print("Protocol: %s %s -> %s" % (
-            ip_header.protocol,
-            ip_header.src_address,
-            ip_header.dst_address)
-        )
-        counter += 1
-
-except KeyboardInterrupt:
-    # if we're on Windows turn off promiscuous mode
-    if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
